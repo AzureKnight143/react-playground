5273ecce042d9922da603b9af2b1a05075e80f05
Matt Wright <matt.wright@corebts.com>
2021-10-11 10:32:23 -0400

    added login form

--- src/home/home.js
+++ src/home/home.js
@@ -1,5 +1,49 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 
-export default function Home() {
-    return <h2>Home</h2>;
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      validated: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    if (event.currentTarget.checkValidity() === false) {
+      event.preventDefault();
+    } else alert(this.state.email + " " + this.state.password);
+
+    this.setState({ validated: true });
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <h2>Home</h2>
+        <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter Email" required value={this.state.email} onChange={this.handleChange} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter Password" required value={this.state.password} onChange={this.handleChange} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
--- src/home/home.test.js
+++ src/home/home.test.js
@@ -1,8 +1,51 @@
-import { render, screen } from '@testing-library/react';
-import Home from './home';
+import { screen, render, fireEvent } from "@testing-library/react";
+import Home from "./home";
 
-it('has a title', () => {
-    render(<Home />);
-    const title = screen.getByText("Home");
-    expect(title).toBeInTheDocument();
+let container;
+beforeEach(() => {
+  ({ container } = render(<Home />));
+});
+
+it("has a title", () => {
+  expect(screen.getByText("Home")).toBeInTheDocument();
+});
+
+it("has a email input", () => {
+  expect(screen.getByLabelText("Email")).toHaveValue("");
+});
+
+it("has a password input", () => {
+  expect(screen.getByLabelText("Password")).toHaveValue("");
+});
+
+it("has a submit button", () => {
+  expect(screen.getByRole("button", { type: "submit" }));
+});
+
+describe("Name of the group", () => {
+  it("handles valid form submit", () => {
+    const alert = jest.spyOn(window, "alert");
+    const email = "test@test.com";
+    const password = "123";
+
+    fireEvent.change(screen.getByLabelText("Email"), { target: { value: email } });
+    fireEvent.change(screen.getByLabelText("Password"), { target: { value: password } });
+    fireEvent.click(screen.getByRole("button", { type: "submit" }));
+
+    expect(alert).toHaveBeenCalledWith(email + " " + password);
+    expect(container.querySelector("form.was-validated")).toBeInTheDocument();
   });
+
+  it("handles invalid form submit ", () => {
+    const alert = jest.spyOn(window, "alert");
+    const email = "test";
+    const password = "";
+
+    fireEvent.change(screen.getByLabelText("Email"), { target: { value: email } });
+    fireEvent.change(screen.getByLabelText("Password"), { target: { value: password } });
+    fireEvent.click(screen.getByRole("button", { type: "submit" }));
+
+    expect(alert).not.toHaveBeenCalled();
+    expect(container.querySelector("form.was-validated")).toBeInTheDocument();
+  });
+});
